58e6d5202c611cbbe583761fe1792f68f8352add
MohitMandal1099 <https://github.com/MohitMandal1099/OOPSBannerApp.git>
2026-03-18 11:06:38 +0530

    added new feature

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,5 @@
 public class HelloApp {
     public static void main(String[] args){
-        System.out.println("Hello, My name is mohit");
+        System.out.println("Hello, My name is mohit kumar mandal");
     }
 }
