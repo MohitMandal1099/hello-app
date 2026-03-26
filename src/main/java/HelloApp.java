40eaf0b26cfc47bde6b20e778d982ce89103d6e8
MohitMandal1099 <https://github.com/MohitMandal1099/OOPSBannerApp.git>
2026-03-26 09:00:30 +0530

    new uc

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,15 +1,19 @@
 public class HelloApp {
-
     public static void main(String[] args) {
+        StringBuilder names = new StringBuilder();
 
-        if(args.length > 0) {
-            String names = String.join(", ", args);
-            System.out.println("Hello, " + names + "!");
-        }
-        else {
-            System.out.println("Hello,World!");
+        if (args.length == 0) {
+            names.append("World");
+        } else {
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
         }
 
+        System.out.println("Hello, " + names.toString() + "!");
     }
-
 }
+
